dff1a43d81c38f997691a2a47f8b2f7fa938843a
Gaurav Bhasin <gaurav.bhasin@aveva.com>
2020-07-14 18:21:02 +0530

    dfs

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var AWS =require ('aws-sdk');
 AWS.config.region='us-east-2c';
-<<<<<<< HEAD
 var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
-=======
->>>>>>> 389d4be750aad90585bc17e68c4e599bd533d2fc
 var elasticache = new AWS.ElastiCache('memcached.w7sebn.cfg.usw2.cache.amazonaws.com:11211');
 var params = {
   ResourceName: 'STRING_VALUE', /* required */
