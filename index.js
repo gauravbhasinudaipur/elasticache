be7671c945cb91952fe9f5c69f122bac8c16322b
Gaurav Bhasin <gaurav.bhasin@aveva.com>
2020-07-14 17:56:17 +0530

    ds

---
+++ index.js
@@ -0,0 +1,17 @@
+var AWS =require ('aws-sdk');
+AWS.config.region='us-east-2c';
+var elasticache = new AWS.ElastiCache('memcached.w7sebn.cfg.usw2.cache.amazonaws.com:11211');
+var params = {
+  ResourceName: 'STRING_VALUE', /* required */
+  Tags: [ /* required */
+    {
+      Key: 'abc',
+      Value: 'aa'
+    },
+    /* more items */
+  ]
+};
+elasticache.addTagsToResource(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
